94c1650c597bf38b71e02397a109ceb6c730ea40
Paige Tingey <paigetingey@Paiges-MacBook-Pro.local>
2017-01-13 10:17:22 -0700

    Configured custom routes for pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,6 +25,7 @@ end
 
 def update
 	@portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -34,4 +35,23 @@ def update
     end
   end
 
+  def show 
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+def destroy
+  # Perform the lookup
+  @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+  end
+
 end
+
+
